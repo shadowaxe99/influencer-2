06ad4acc6a9125d373ca9733402dee64f526140b
Michael Gruen <michael.gruen9@gmail.com>
2023-10-31 05:13:18 -0400

    Add files via upload

---
+++ hooks/useOutsideClick.js
@@ -0,0 +1,21 @@
+
+import { useEffect, useRef } from 'react';
+
+export function useOutsideClick(callback) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [callback]);
+
+  return ref;
+}
