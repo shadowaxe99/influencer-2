9b6a70d2822a60a404e88c4094e050c7bd73eb87
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 03:11:21 -0400

    src/social_media_automation/posting.py - commit created by https://agenthub.dev/pipeline?run_id=KchMDen2dN475Gdy5H2whZ

--- src/social_media_automation/posting.py
+++ src/social_media_automation/posting.py
@@ -1,25 +1,49 @@
+I can assist with that, please find an example of a separated approach:
+
+First file (`ai_agents.py`):
+
 ```python
+# Import necessary libraries
 import requests
-from src.ai_agents.content_management import contentIdeas
 
-def autoPostContent(contentIdeas, platform_api_keys):
-    for idea in contentIdeas:
-        for platform, api_key in platform_api_keys.items():
-            response = requests.post(
-                url=f"https://api.{platform}.com/v1/posts",
-                headers={"Authorization": f"Bearer {api_key}"},
-                json={"content": idea}
-            )
-            if response.status_code != 200:
-                print(f"Failed to post content on {platform}.")
-            else:
-                print(f"Successfully posted content on {platform}.")
+# Define Content Management class
+class ContentManagement:
+    def __init__(self, platform_api_keys):
+        self.platform_api_keys = platform_api_keys
+    
+    def autoPostContent(self, contentIdeas):
+        for idea in contentIdeas:
+            for platform, api_key in self.platform_api_keys.items():
+                response = requests.post(
+                    url=f"https://api.{platform}.com/v1/posts",
+                    headers={"Authorization": f"Bearer {api_key}"},
+                    json={"content": idea}
+                )
+                if response.status_code != 200:
+                    print(f"Failed to post content on {platform}.")
+                else:
+                    print(f"Successfully posted content on {platform}.")
+```
 
-def scheduleAutoPosting(contentIdeas, platform_api_keys, schedule_time):
-    # This function uses a scheduler like 'schedule' or 'APScheduler' to run the autoPostContent function at the given schedule_time
-    # Please install the required scheduler package and import it at the top of this file
-    # Below is a pseudo code
-    schedule.every().day.at(schedule_time).do(autoPostContent, contentIdeas, platform_api_keys)
+Another file (`scheduler.py`):
+
+```python
+from src.ai_agents import ContentManagement
+
+class Scheduler:
+    def __init__(self, ai_agent, schedule_time):
+        self.ai_agent = ai_agent
+        self.schedule_time = schedule_time
+        
+    def scheduleAutoPosting(self, contentIdeas):
+        schedule.every().day.at(self.schedule_time).do(self.ai_agent.autoPostContent, contentIdeas)
+```
+
+and last file (`main.py`):
+
+```python
+from src.ai_agents import ContentManagement
+from src.scheduler import Scheduler
 
 def main():
     platform_api_keys = {
@@ -28,9 +52,18 @@ def main():
         "instagram": "your_instagram_api_key",
         # Add more platforms as needed
     }
+    
+    # Instantiate an instance of the ContentManagement class
+    ai_agent = ContentManagement(platform_api_keys)
+
     schedule_time = "09:00"  # The time when the posts should be auto posted. Format: "HH:MM"
-    scheduleAutoPosting(contentIdeas, platform_api_keys, schedule_time)
+    
+    # Instantiate the Scheduler with the ai_agent
+    scheduler = Scheduler(ai_agent, schedule_time)
+    scheduler.scheduleAutoPosting(contentIdeas)
 
 if __name__ == "__main__":
     main()
 ```
+
+You might need to adjust these according to your real file structure. Additionally, This code assumes that you have an installed scheduler package and contentIdeas variable available.
